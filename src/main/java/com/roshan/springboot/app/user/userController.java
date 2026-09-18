6fd463934ed9da479e299b32838f8d5476c3b561
roshan <mrroshan738949@gmail.com>
2026-09-18 18:30:35 +0530

    adding custom properties in the app

--- src/main/java/com/roshan/springboot/app/user/userController.java
+++ src/main/java/com/roshan/springboot/app/user/userController.java
@@ -1,14 +1,28 @@
 package com.roshan.springboot.app.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class userController {
 
+    // inject properties
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${user.surname}")
+    private String userSurname;
+
+    // exposing the "/" endpoint
     @GetMapping("/")
-    public String sayHello(){
+    public String sayHello() {
         return "Hello roshan";
+    }
+
+    @GetMapping("/userinfo")
+    public String getUserInfo(){
+        return userName + " " + userSurname;
     }
 
     @GetMapping("/workout")
