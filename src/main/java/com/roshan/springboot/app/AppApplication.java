9dc736faba33ca8efe2862cf1daa9cf7bef904c7
roshan <mrroshan738949@gmail.com>
2026-09-19 22:24:48 +0530

    feat : scanning the packages outside the packages directory

--- src/main/java/com/roshan/springboot/app/AppApplication.java
+++ src/main/java/com/roshan/springboot/app/AppApplication.java
@@ -3,6 +3,11 @@ package com.roshan.springboot.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Scanning external packages
+//@SpringBootApplication(
+//		scanBasePackages = {"com.roshan.util" , "com.roshan.springboot.app"}
+//)
+
 @SpringBootApplication
 public class AppApplication {
 
